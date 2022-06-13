220dcd79ca407966c775e267af56259e7c4c0943
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-13 23:40:49 +0200

    fix compilation location

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 const app = express();
 const port = 5000;
 app.get("/", (_, res) => {
-	res.status(200).send();
+	res.status(200).send("hello");
 });
-app.listen(port, () => console.log(`Running on port ${port}`));
+app.listen(port, () => console.log(`Running on port hahha ${port}`));
