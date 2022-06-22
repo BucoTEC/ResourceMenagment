45efad01fa44d8f8835680de9d9ac49720c5321f
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-22 20:43:34 +0200

    branch setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express from "express";
+import express from 'express';
 const app = express();
 const port = 5000;
-app.get("/", (_, res) => {
-	res.status(200).send("hello");
+app.get('/', (_, res) => {
+  res.status(200).send('hello');
 });
-app.listen(port, () => console.log(`Running on port hahha ${port}`));
+app.listen(port, () => console.log(`Running on port hahha test ${port}`));
