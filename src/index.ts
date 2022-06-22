92c9408b4e141e3692675d37eab30d650a3a1049
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-06-22 21:00:13 +0200

    setup main starter file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,47 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import express from 'express';
-const app = express();
-const port = 5000;
-app.get('/', (_, res) => {
-  res.status(200).send('hello');
-});
-app.listen(port, () => console.log(`Running on port hahha test ${port}`));
+import http from 'http';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'world';
+    }
+  }
+};
+
+async function listen(port: number) {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+  });
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  return new Promise((resolve, reject) => {
+    httpServer.listen(port).once('listening', resolve).once('error', reject);
+  });
+}
+
+async function main() {
+  try {
+    await listen(4000);
+    console.log('🚀 Server is ready at http://localhost:4000/graphql');
+  } catch (err) {
+    console.error('💀 Error starting the node server', err);
+  }
+}
+
+void main();
